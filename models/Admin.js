ae09d84b9f16342570ee84ecd96d5627c42335d1
unknown <72mo2v@gmail.com>
2025-11-20 13:35:30 -0800

    first commit

--- models/Admin.js
+++ models/Admin.js
@@ -3,7 +3,7 @@
 // ======================================
 
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
+const { sequelize } = require('../config/database');
 const bcrypt = require('bcrypt');
 
 const Admin = sequelize.define('Admin', {
